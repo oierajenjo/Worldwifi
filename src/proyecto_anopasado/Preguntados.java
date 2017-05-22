43e0ef1d19a49abfa43052a05648adfa71720c26
oierajenjo <oierajenjo@opendeusto.es>
2017-05-22 12:01:33 +0200

    Lo del proyecto de Prog2

---
+++ src/proyecto_anopasado/Preguntados.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+public class Preguntados {
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+//Preguntados: clase principal de lógica del proyecto
+//•Atributos: lista de partidas
--- src/usuario/InicioSesion.java
+++ src/usuario/InicioSesion.java
@@ -1,6 +1,16 @@
 package usuario;
 
+import javafx.scene.chart.PieChart.Data;
+
 public class InicioSesion extends NickUsuario {
+
+	public InicioSesion(String nombreUsuario, String nombre, String apellido, int numTelef, Data fechaNacimiento,
+			String correo, String contrasenya, String repiteContrasenya, String twitter, String facebook,
+			TipoUsuario tipo, long fechaUltimoLogin, String nickName, String nickContrasenya) {
+		super(nombreUsuario, nombre, apellido, numTelef, fechaNacimiento, correo, contrasenya, repiteContrasenya, twitter,
+				facebook, tipo, fechaUltimoLogin, nickName, nickContrasenya);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
